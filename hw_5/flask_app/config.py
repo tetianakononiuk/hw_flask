6beec7cbb99227b52ef1bf7c7d71709e81b65284
Tetiana <tetianakononiuk0705@gmail.com>
2024-09-04 23:44:45 +0200

    added hw_5

---
+++ hw_5/flask_app/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Users/ICH/PycharmProjects/flask_project_1/flask_app/app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ hw_5/flask_app/models.py
@@ -0,0 +1,23 @@
+from . import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+    def __str__(self):
+        return f'Category {self.name}'
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    text = db.Column(db.String(500), nullable=False)
+    answer = db.Column(db.String(500), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=True)
+
+    category = db.relationship('Category', backref=db.backref('questions', lazy=True))
+
+    def __str__(self):
+        return f'Question {self.text}'
